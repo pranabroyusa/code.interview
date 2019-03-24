a26e0c78d28621f483a03bdd03753bd3e2b41ea0
Pranab Roy <Pranab Roy@Pranab.home>
2019-03-24 08:20:14 -0700

    Completed fibonacci coding

--- src/main/java/math/Fabonachi_Number.java
+++ src/main/java/math/Fabonachi_Number.java
@@ -1,9 +1,23 @@
 package math;
 
+import org.testng.annotations.Test;
+
 public class Fabonachi_Number {
   @Test
   public void fibonacci()
   {
+	  int a=0;
+	  int b=1;
+	  int c;
+	  System.out.println("-----"+a);
+	  System.out.println("-----"+b);
+	  for(int i=0;i<10;i++)
+	  {
+		c=a+b;
+		a=b;
+		b=c;
+		System.out.println("-----"+c);
+	  }
   }
 
 }
