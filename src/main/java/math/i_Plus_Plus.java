6d03e39173f0969854b33559c2a449d7e165f9b0
Pranab Roy <Pranab Roy@Pranab.home>
2019-03-23 23:19:16 -0700

    i++ program is added

---
+++ src/main/java/math/i_Plus_Plus.java
@@ -0,0 +1,26 @@
+package math;
+
+import org.testng.annotations.Test;
+
+public class i_Plus_Plus {
+	@Test
+	public void m1()
+	{
+		int i=1;
+		int j=++i;
+		System.out.println("Value of i :"+i+" Value of j :"+j);
+		
+		
+	}
+	
+	@Test
+	public void m2()
+	{
+		int i=1;
+		int j=i++;
+		System.out.println("Value of i :"+i+" Value of j :"+j);
+		
+		
+	}
+
+}
