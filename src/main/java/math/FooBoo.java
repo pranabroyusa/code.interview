54c380d1158237a79a9ad1f8ed9cd956f98dddb3
pranabroyusa <pranabroyusa@gmail.com>
2019-03-24 05:37:26 -0700

    Update FooBoo.java

--- src/main/java/math/FooBoo.java
+++ src/main/java/math/FooBoo.java
@@ -1,5 +1,26 @@
 package math;
 
 public class FooBoo {
+  @Test
+  public voif remainder()
+  {
+    int x=13;
+    if(x%3==0)
+    {
+      System.out.println("--- Foo");
+        if(x%5==0)
+    {
+      System.out.println("--- FooBoo");
+    }
+     else   if(x%5==0)
+         {
+      System.out.println("--- Boo");
+    }
+    }
+    
+   
+    
+    
+  }
 
 }
